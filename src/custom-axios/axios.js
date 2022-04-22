98ae025cd6143f57e8e835213135ae1d8886d24b
Anastasija Gjosheva <anastasija.gjosheva@students.finki.ukim.mk>
2022-04-22 15:21:57 +0200

    baseURL changed

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9090/api',
+    baseURL: 'https://emtlabfrontend193049.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin' : '*',
 
